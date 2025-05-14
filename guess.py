54bac049a01c60ce903ecd6c06cf43a614a8667c
tabassum-an <ktabassum4516@gmail.com>
2025-05-14 20:21:42 +0600

    Update guess.py

--- guess.py
+++ guess.py
@@ -12,28 +12,7 @@ def game_start():
     else:
         print("invalid input")
         return
-# # rule 1 with game_over variable
-#     game_over = False
-#     while not game_over:
-#         print(f"You have {num_of_guesses} attempts remaining to guess the number.")
-#         guess = int(input("Make a guess: "))
-#
-#         if guess == number:
-#             print(f"You got it! The answer was {number}")
-#             return
-#         elif guess > number:
-#             print(f"Too high.\nGuess again.")
-#         else:
-#             print(f"Too low.\nGuess again.")
-#
-#         num_of_guesses -= 1
-#         if num_of_guesses == 0:
-#             game_over = True
-#             print(f"You've run out of guesses. The number was {number}")
-#
-# game_start()
 
-# rule 2 more direct approach
     while num_of_guesses > 0:
         print(f"\nYou have {num_of_guesses} attempts remaining to guess the number.")
         try:
